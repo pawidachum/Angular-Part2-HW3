86e64cf04b64cdc9997141a51b78b68d694cf347
pawidachum <pawida.may@gmail.com>
2021-09-06 10:28:01 +0700

    do search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ConfigComponent } from './config/config.component';
+import { ModelComponent } from './model/model.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConfigComponent,
+    ModelComponent
   ],
   imports: [
     BrowserModule
